c7c9684a4cb0c86537e046e93edc58d8df4f1052
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-05-22 13:10:28 +0300

    repo class created

--- app/src/main/java/com/wifianalyzer/wifianalyzerproject/repository/RssiSignalsRepo.kt
+++ app/src/main/java/com/wifianalyzer/wifianalyzerproject/repository/RssiSignalsRepo.kt
@@ -3,15 +3,16 @@ package com.wifianalyzer.wifianalyzerproject.repository
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.database.FirebaseDatabase
-import com.wifianalyzer.wifianalyzerproject.data.RssiSignalData
+import com.google.firebase.ktx.Firebase
+import com.wifianalyzer.wifianalyzerproject.data.RssiSignal
 
 class RssiSignalsRepo {
 
-    val rssiSignalList : MutableLiveData<List<RssiSignalData>> = MutableLiveData()
+    val rssiSignalList : MutableLiveData<List<RssiSignal>> = MutableLiveData()
     val dbRefRssiSignal = FirebaseDatabase.getInstance().getReference("rssiSignals")
 
-    fun insertRssiSignal(obj: RssiSignalData, userkey: String, currentTimestamp: Long){
-        val db = dbRefRssiSignal.child(userkey).child(currentTimestamp.toString())
+    fun insertRssiSignal(obj:RssiSignal,timestamp:Long){
+        val db = dbRefRssiSignal.child(timestamp.toString())
         db.push().setValue(obj).addOnFailureListener { it ->
             Log.e("hata: ", it.toString())
         }
